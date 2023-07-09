fa3d1efca92e89d04e35d2b32112291cb427ff95
fancymithun <callmeprabhu7@gmail.com>
2023-07-09 16:57:28 +0530

    Updated linked list ds

---
+++ DataStructures/linkedList.js
@@ -0,0 +1,35 @@
+class Node{
+    constructor(data){
+        this.data = data
+        this.next = null
+    }
+}
+
+class LinkedList{
+    constructor(){
+        this.root = null
+    }
+
+    addNode(data){
+        const newNode = new Node(data)
+
+        if(!this.root){
+            this.root = newNode
+        }else{
+            let currentNode = this.root
+            while(currentNode.next){
+                currentNode = currentNode.next
+            }
+            currentNode.next = newNode
+        }
+    }
+
+}
+
+const ll = new LinkedList()
+ll.addNode(1)
+ll.addNode(2)
+ll.addNode(3)
+ll.addNode(4)
+
+console.log(ll)
